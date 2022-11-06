faedf1e5e09491c63793e7dedb1da01d72039cbb
Austin Flynn <43562703+aflynn93@users.noreply.github.com>
2022-11-05 21:59:04 -0500

    Completed part 11
    Revert back to BrowserRouter.
    
    Use redirect logic in /public/index.html
    
    Use Single Page App for GitHub Pages in /public/404.html

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { HashRouter } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom"
 
 import TodoContainer from "./functionBased//components/TodoContainer"
 import "./functionBased/App.css"
 
 ReactDOM.render(
     <React.StrictMode>
-        <HashRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <TodoContainer />
-        </HashRouter>
+        </BrowserRouter>
     </React.StrictMode>,
     document.getElementById("root")
 )
